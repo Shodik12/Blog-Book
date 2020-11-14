c7c2e666431e9cb323cd391a8a933a3589818f25
Shodik12 <67968446+Shodik12@users.noreply.github.com>
2020-11-14 19:07:51 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,13 +2,13 @@ export const environment = {
   production: true,
 
   firebaseConfig :{
- apiKey: "AIzaSyDvmru25CvXLIu67jgLCx0WwJNR2EXRSks",
- authDomain: "bloger-aa71f.firebaseapp.com",
- databaseURL: "https://bloger-aa71f.firebaseio.com",
- projectId: "bloger-aa71f",
- storageBucket: "bloger-aa71f.appspot.com",
- messagingSenderId: "409239857485",
- appId: "1:409239857485:web:20f3c97a557586545a6f04",
- measurementId: "G-WW810JH35Q"
+ apiKey: "",
+ authDomain: "",
+ databaseURL: "",
+ projectId: "",
+ storageBucket: "",
+ messagingSenderId: "",
+ appId: "",
+ measurementId: ""
 }
 };
